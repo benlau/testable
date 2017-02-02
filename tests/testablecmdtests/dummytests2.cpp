33bb35a58ae59edfe48321d7caebc4eaa977056d
Ben Lau <xbenlau@gmail.com>
2017-02-02 10:42:51 +0800

    Add docs/screenshot1.png

--- tests/testablecmdtests/dummytests2.cpp
+++ tests/testablecmdtests/dummytests2.cpp
@@ -3,7 +3,10 @@
 
 DummyTests2::DummyTests2(QObject *parent) : QObject(parent)
 {
-
+    auto ref =[=]() {
+        QTest::qExec(this, 0, 0);
+    };
+    Q_UNUSED(ref);
 }
 
 void DummyTests2::test1()
